73fe8d1005738d49496171d8aa8f54bea8d3e8a0
Daniel Nguyen <danielnguyen@dhcp-10-250-126-140.harvard.edu>
2020-02-23 16:27:30 -0500

    test

--- prog1/randmst.cpp
+++ prog1/randmst.cpp
@@ -19,43 +19,52 @@ struct Set{
         rank = 0; 
         parent = this; 
     };    
-    Set* find(){
+
+    Set find(){
+        // printf("%d", this->parent == this); 
         if (this->parent != this){
-            Set* parent = this->parent->find(); 
-            this->parent = parent; 
+            Set parent = this->parent->find(); 
+            this->parent = &parent; 
         }
-        return this; 
+        return *this; 
     };    
 
-    void makeUnion(Set* nodeToUnionWith){
-        Set* parent1 = this->find(); 
-        Set* parent2 = nodeToUnionWith->find(); 
-        parent1->link(parent2); 
+    void makeUnion(Set nodeToUnionWith){
+        
+        Set parent1 = this->find(); 
+        Set parent2 = nodeToUnionWith.find(); 
+        
+        parent1.link(parent2); 
     }; 
     // replace two sets containing x and y by their union.   
-    void link(Set* nodeToLinkWith){
-        Set* smallRankNode = this; 
-        Set* largeRankNode = nodeToLinkWith; 
-        if (smallRankNode->rank > largeRankNode->rank){ 
-            Set* temp = smallRankNode; 
+    void link(Set nodeToLinkWith){
+        
+        Set smallRankNode = *this; 
+        Set largeRankNode = nodeToLinkWith; 
+        
+        if (smallRankNode.rank > largeRankNode.rank){ 
+            Set temp = smallRankNode; 
             smallRankNode = largeRankNode; 
             largeRankNode = temp; 
         }
         // only increase rank when ranks are equal. 
-        if(smallRankNode->rank == largeRankNode->rank){ 
-            largeRankNode->rank++; 
+        if(smallRankNode.rank == largeRankNode.rank){ 
+            largeRankNode.rank++; 
         }
         // point smaller rank node to larger rank node. 
-        smallRankNode->parent = largeRankNode; 
+        smallRankNode.parent = &largeRankNode; 
     }; // connect two nodes with union by rank 
 };
 
 
 
 int main(int argc, char* argv[]){ 
-    std::vector<Set>newSet = {Set(5), Set(4)}; 
-    newSet[0].link(&newSet[1]); 
-    std::cout << newSet[0].find()->vertex << "\n"; 
-    std::cout << newSet[1].find()->vertex << "\n"; 
+    printf("no seg");
+    std::vector<Set>newSet; 
+    newSet.push_back(Set(5)); 
+    newSet.push_back(Set(4)); 
+    newSet[0].makeUnion(newSet[1]); 
+    std::cout << newSet[0].find().vertex << "\n"; 
+    std::cout << newSet[1].find().vertex << "\n"; 
 
 }
